a84c8217fb0d55d7f702d5e8df20370e7cfeba7a
Ivan D. Tarnyagin <ivan.tarnyagin@gmail.com>
2021-04-21 18:31:27 +0200

    work in progress..😋

--- player.js
+++ player.js
@@ -4,17 +4,23 @@ class Player {
     this.y = 10;
     this.size = tileSize;
     this.gravity = 1.005;
-    this.v_speed = 10;
+    this.v_speed = 0;
+    this.onFloor = false;
   }
   move() {
     // gravity
+    if (this.v_speed == 0) {
+      this.v_speed += 10;
+    }
     this.v_speed *= this.gravity;
     this.y += this.v_speed;
 
-    // collition with floor
+    // collision with floor
     if (this.y > f_height - tileSize) {
       this.y = f_height - tileSize;
+      this.v_speed = 0;
     }
+    // check keyboard input
     keyPressed();
   }
   render() {
@@ -24,6 +30,7 @@ class Player {
 }
 
 function keyPressed() {
+  // move left - right
   if (keyIsDown(LEFT_ARROW)) {
     player.x -= 10;
   }
@@ -31,3 +38,5 @@ function keyPressed() {
     player.x += 10;
   }
 }
+
+// *TODO make isOnFloor function, and add vertical velocity function n shit
